c1dc3e94130d50c30db4007dd2303b015e038e72
Jeevikha S <jjeevikha@gmail.com>
2026-01-19 22:30:33 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (67.39%) - LeetSync

---
+++ 400-nth-digit/nth-digit.java
@@ -0,0 +1,15 @@
+public class Solution{
+    public int findNthDigit(int n){
+        long len=1;
+        long count=9;
+        long start=1;
+        while(n>len*count){
+            n-=len*count;
+            len++;
+            count*=10;
+            start*=10;
+        }
+        long num=start+(n-1)/len;
+        return String.valueOf(num).charAt((int)((n-1)%len))-'0';
+    }
+}
